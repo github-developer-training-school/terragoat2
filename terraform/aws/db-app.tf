af4277b6fea8a446caf242374d783da15590b653
Noemi Csokas <noemi.csokas@eficode.com>
2025-09-09 08:36:55 +0200

    tflint annotation test

--- terraform/aws/db-app.tf
+++ terraform/aws/db-app.tf
@@ -5,12 +5,12 @@ resource "aws_db_instance" "default" {
   option_group_name      = aws_db_option_group.default.name
   parameter_group_name   = aws_db_parameter_group.default.name
   db_subnet_group_name   = aws_db_subnet_group.default.name
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  vpc_security_group_ids = [aws_security_group.default.id]
 
   identifier              = "rds-${local.resource_prefix.value}"
   engine_version          = "8.0" # Latest major version 
   instance_class          = "db.t3.micro"
-  allocated_storage       = "20"
+  allocated_storage       = 20
   username                = "admin"
   password                = var.password
   apply_immediately       = true
@@ -37,7 +37,7 @@ resource "aws_db_instance" "default" {
 
   # Ignore password changes from tf plan diff
   lifecycle {
-    ignore_changes = ["password"]
+    ignore_changes = [password]
   }
 }
 
